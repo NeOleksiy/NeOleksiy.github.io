e2d55af7a65ad8b43f6c97b8ed0e155a20494c73
alyefim <alyefim@gmail.com>
2022-06-26 23:50:57 +0300

    1_commit

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['POST', 'GET'])
+def hello_world():
+    return render_template ("main.html")
+
+
+@app.route('/project', methods=['POST', 'GET'])
+def Project_f():
+    return render_template ("Project.html")
 
 
 if __name__ == '__main__':
